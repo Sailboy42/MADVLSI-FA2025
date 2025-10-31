006abbe33ab8a1ff959d4c3abaf47a63493d7032
Sailboy42 <123433681+Sailboy42@users.noreply.github.com>
2025-10-31 16:56:32 -0400

    Add Layout FIles

---
+++ LVSv1.tcl
@@ -0,0 +1,32 @@
+loading history file ... 1 events added
+Running NetGen Console Functions
+Netgen 1.5.299 compiled on Sat Sep  6 02:50:48 PM EDT 2025
+Warning: netgen command 'format' use fully-qualified name '::netgen::format'
+Warning: netgen command 'global' use fully-qualified name '::netgen::global'
+Main console display active (Tcl8.6.14 / Tk8.6.14)
+(layout) 2 % lvs Sketches.spice ieee_DAC.spice
+Reading netlist file Sketches.spice
+Call to undefined subcircuit sky130_fd_pr__nfet_01v8
+Creating placeholder cell definition.
+Call to undefined subcircuit inv
+Creating placeholder cell definition.
+Ignoring line starting with token: .save
+line number 78 = '.save i(vib)'
+Ignoring line starting with token: .option
+line number 80 = ' .option wnflag=1'
+Ignoring line starting with token: .lib
+line number 82 = '.lib /usr/local/share/pdk/sky130A/libs.tech/ngspice/sky130.lib.spice tt'
+Ignoring line starting with token: .save
+line number 84 = '.save all'
+Call to undefined subcircuit sky130_fd_pr__pfet_01v8
+Creating placeholder cell definition.
+Cell Sketches.spice:  Net GND changed to global
+Cell Sketches.spice:  Net vdd changed to global
+File Sketches.spice read with 4 warnings.
+Reading netlist file ieee_DAC.spice
+Call to undefined subcircuit sky130_fd_pr__nfet_01v8
+Creating placeholder cell definition.
+
+Error:  Setup file setup.tcl does not exist.
+
+(layout) 3 % 
